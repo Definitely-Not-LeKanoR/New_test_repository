4fc3986fdd22234d82f74f9272592203ae856f25
LeKanoR <moroz-moroz2005@mail.ru>
2025-04-12 09:25:29 +0300

    Изменена реализация пузырьковой сортировки на более читабельный код

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -1,1 +1,29 @@
-"using System; class Program { static void Main(string[] args) { int[] array = { 5, 3, 8, 4, 2 }; BubbleSort(array); Console.WriteLine(string.Join(", ", array)); } static void BubbleSort(int[] arr) { int n = arr.Length; for (int i = 0; i < n - 1; i++) { for (int j = 0; j < n - i - 1; j++) { if (arr[j] > arr[j + 1]) { // ¥­ο¥¬ arr[j] ¨ arr[j+1] int temp = arr[j]; arr[j] = arr[j + 1]; arr[j + 1] = temp; } } } } }" 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 3, 8, 4, 2 };
+        BubbleSort(array);
+        Console.WriteLine(string.Join(", ", array));
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Ξαμεν ηνΰχενθÿμθ
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
